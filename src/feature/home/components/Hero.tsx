f455a678d3cda681c79258fb1818d94fdc52a75f
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-07-06 18:01:45 +0700

    feat: add Hero component and update HomePage layout; enhance Footer styling

---
+++ src/feature/home/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section>
+      <img
+        src="/src/assets/images/hero-placeholder.png"
+        alt="image"
+        className="w-full"
+      />
+    </section>
+  );
+};
+
+export default Hero;
--- src/feature/home/section/HomePage.tsx
+++ src/feature/home/section/HomePage.tsx
@@ -1,5 +1,11 @@
+import Hero from "../components/Hero.tsx";
+
 const HomePage = () => {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="min-h-screen">
+      <Hero />
+    </div>
+  );
 };
 
 export default HomePage;
